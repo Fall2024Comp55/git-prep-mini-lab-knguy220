d4863b0d0ebe26d2ee562793980901ecbd74a6ea
Kennedy Nguyen <k_nguyen198@u.pacific.edu>
2024-10-02 13:28:05 -0700

    12

--- COMP55GitPrepLab/src/SimplePicture.java
+++ COMP55GitPrepLab/src/SimplePicture.java
@@ -13,7 +13,7 @@ public class SimplePicture extends GraphicsProgram {
 	public void run() {
 		GImage robot = new GImage("robot.jpg", 200, 100);
 		add(robot);
-		GLabel label = new GLabel("1How I felt before knowing about github", 200, 400);
+		GLabel label = new GLabel("12How I felt before knowing about github", 200, 400);
 		add(label);
 	}
 	
